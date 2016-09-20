e4e683b514957ca2e1504262b61d86d418015ca9
IamTobi <contact@iamtobi.eu>
2016-09-20 22:18:11 +0200

    gameboard created, grid positioning initial implemented

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    /// <summary>
+    /// GameManager used for Initialisation and keeping Track of score and positions
+    /// </summary>
+    public class GameManager : MonoBehaviour {
+
+        // Use this for initialization
+        void Start () {
+	
+        }
+	
+        // Update is called once per frame
+        void Update () {
+	
+        }
+    }
+}
